8d9de434d4f185268eb60d22d2b4a1b3a39edf3a
Ricardo <ricardo.bav17@gmail.com>
2022-12-29 18:19:48 -0300

    definicao do titulo por paginas

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,12 @@
+import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
 export default function Inicial() {
   return (
     <>
+      <Head>
+        <title>Página principal</title>    
+      </Head>       
       <h1> Página Inicial</h1>      
     </>
   )
--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,7 +1,12 @@
+import Head from 'next/head'
+
 export default function Login(){
 
     return (
-        <>        
+        <> 
+            <Head>
+                <title>Página de login</title>    
+            </Head>       
             <h1>Login</h1>        
         </>
     )
