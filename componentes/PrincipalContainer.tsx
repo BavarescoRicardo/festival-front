fe7d95ce30733e07aef75af71dae3bcf69d2faa8
Ricardo Bavaresco <ricardo.bav17@gmail.com>
2023-01-13 18:08:50 -0300

    adicionado meta tag para dimensionar tela para responsivo

--- componentes/PrincipalContainer.tsx
+++ componentes/PrincipalContainer.tsx
@@ -1,9 +1,13 @@
 import React, { ReactNode } from "react";
+import Head from 'next/head'
 import BarraNavegacao from "./BarraNavegacao";
 
 export default function PrincipalContainer({children}) {
     return(
         <>
+            <Head>
+                <meta name="viewport" content="width=device-width, initial-scale=1" />   
+            </Head> 
             <BarraNavegacao />
             <div>{children}</div>
         </>
