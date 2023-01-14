61a57f600fd3073c0f09363ef5fb294a32af2884
Ricardo Bavaresco <ricardo.bav17@gmail.com>
2023-01-14 13:46:32 -0300

    ajuste estilo paginacao

--- componentes/Paginacao.tsx
+++ componentes/Paginacao.tsx
@@ -1,34 +1,34 @@
 import { useState } from 'react'
+import styles from '../styles/Paginacao.module.css'
 
 export default function Paginacao(props: any) {
     const [showModal, setShowModal] = useState(false);
+    const [participante, setParticipante] = useState(0);
+
     return (
 
-        <nav aria-label="Page navigation example">
-        <ul className="inline-flex -space-x-px">
-            <li>
-            <a href="#" className="px-3 py-2 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Previous</a>
-            </li>
-            <li>
-            <a href="#" className="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">1</a>
-            </li>
-            <li>
-            <a href="#" className="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">2</a>
-            </li>
-            <li>
-            <a href="#" aria-current="page" className="px-3 py-2 text-blue-600 border border-gray-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white">3</a>
-            </li>
-            <li>
-            <a href="#" className="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">4</a>
-            </li>
-            <li>
-            <a href="#" className="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">5</a>
-            </li>
-            <li>
-            <a href="#" className="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Next</a>
-            </li>
-        </ul>
-        </nav>
+        <div className="flex justify-center">
+            <nav aria-label="Page navigation example">
+                <ul className={styles.page}>
+                    <li className={styles.pageitem}><a
+                        className=""
+                        href="#">1</a></li>
+                    <li className={styles.pageitem}><a
+                        className=""
+                        href="#">2</a></li>
+                    <li className={styles.pageitem}><a
+                        className=""
+                        href="#">3</a></li>
+                    <li className={styles.pageitem}><a
+                        className=""
+                        href="#">4</a></li>
+                    <li className={styles.pageitem}><a
+                        className=""
+                        href="#">5</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
 
     )
 }
