f51cad195a3b3833bd2324d644a2b68997d88b79
Ricardo Bavaresco <ricardo.bav17@gmail.com>
2023-01-14 01:13:47 -0300

    adicionadas fotos divulgacao fimusi, edicao estilo do indice

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,51 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Inicial.module.css'
 
 export default function Inicial() {
   return (
     <>
       <Head>
         <title>Página principal</title>    
-      </Head>       
-      <h1> Página Inicial</h1>      
+      </Head>
+      <div className={styles.container}>
+        <div className={styles.titulo}>
+          <h1> Página Inicial</h1>
+        </div>
+
+        <div className={styles.description}>
+          <div className={styles.fotos}>
+
+          </div>
+
+          <div className={styles.fotos}>
+
+          </div>
+
+          <div className={styles.fotos}>
+
+          </div>
+
+          <div className={styles.fotos}>
+
+          </div>
+
+          <div className={styles.fotos}>
+
+          </div>
+
+          <div className={styles.fotos}>
+
+          </div>
+
+          <div className={styles.fotos}>
+
+          </div>
+
+          <div className={styles.fotos}>
+
+          </div>          
+        </div>
+      </div>   
     </>
   )
 }
